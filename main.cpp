810c32550578e1a3523b86964b949519865c0a6f
Biszek <226056@student.pwr.edu.pl>
2016-03-31 00:39:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,44 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	int n,m;
+	
+	scanf("%d%d",&n,&m);
+	srand(m);
+	
 	drawCharSquare(charSquare(n), n);	
+
 }
 
 char **charSquare(int n)
 {
 	char **square;
+	
+	square = (char**) malloc(n*sizeof(char*));
+	
+	for(int i=0;i<n;i++)
+	square[i] = (char*) malloc(n*sizeof(char));
+	
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+		square[i][j]=char((rand()%26)+97);	
+
 	return square;
-//lalalalalala
+
 }
 
 void drawCharSquare(char **square, int n)
 {
+	for(int i=0;i<n;i++)
+		{
+		for(int j=0;j<n;j++)
+		printf("%c",square[i][j]);
+		printf("\n");
+		}
+
+	for(int i=0;i<n;i++)
+	free(square[i]);
+
+	free(square);
 
 }
